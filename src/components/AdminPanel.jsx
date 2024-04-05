40ca204058807e6fad0f6cedb611ae0269359a41
Artem Zakharov <zaxarov89248742329@gmail.com>
2024-04-05 18:29:38 +0700

    add changeRunLine

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,90 +1,190 @@
-import { useState, useEffect } from 'react';
-import './styles/AdminPanel.css';
-import runLine from '../data/runLine.json';
-import TeamAdmin from './TeamAdmin';
+import { useState, useEffect } from "react";
+import "./styles/AdminPanel.css";
+import TeamAdmin from "./TeamAdmin";
 
 function AdminPanel() {
+  const [data, setData] = useState(
+    JSON.parse(localStorage.getItem("teamData"))
+  );
+  const [runLine, setRunLine] = useState(false);
+  const [textRunLine, setTextRunLine] = useState(
+    JSON.parse(localStorage.getItem("runLine"))
+  );
+  const [ChangetextRunLine, setChangetextRunLine] = useState(textRunLine);
+  const [inputValue, setInputValue] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [scrollAmount, setScrollAmount] = useState("10");
 
-    const [data, setData] = useState(JSON.parse(localStorage.getItem('teamData')));
-    const [textRunLine, settextRunLine] = useState(runLine)
-    const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    const ChangePoints = (teamId, point) => {
-        const newData = data.map(e => e.id === teamId ? {...e, point} : {...e, point: e.point})
-        setData(newData);
+  const ChangePoints = (teamId, point) => {
+    const newData = data.map((e) => (e.id === teamId ? { ...e, point } : e));
+    setData(newData);
 
-        localStorage.removeItem('teamData');
-        localStorage.setItem('teamData', JSON.stringify(newData));
-    
-    }
+    localStorage.removeItem("teamData");
+    localStorage.setItem("teamData", JSON.stringify(newData));
+  };
 
-    const DeleteTeam = (teamId) => {
-        const newData = data.filter(e => e.id !== teamId);
-        setData(newData);
+  const ChangeAvatar = (teamId, image) => {
+    const newData = data.map((e) => (e.id === teamId ? { ...e, image } : e));
+    setData(newData);
 
-        localStorage.removeItem('teamData');
-        localStorage.setItem('teamData', JSON.stringify(newData));
-    }
+    localStorage.removeItem("teamData");
+    localStorage.setItem("teamData", JSON.stringify(newData));
+  };
 
-    const handleAddTeam = () => {
-        const teamData = JSON.parse(localStorage.getItem('teamData')) || []; 
-        const newTeam = {
-            id: teamData.length + 1, 
-            name: inputValue,
-            point: 0 
-        };
-        teamData.push(newTeam); 
-        setData(teamData);
-        setInputValue('');
-        localStorage.removeItem('teamData');
-        localStorage.setItem('teamData', JSON.stringify(teamData));
-    }
+  const DeleteTeam = (teamId) => {
+    const newData = data.filter((e) => e.id !== teamId);
+    setData(newData);
 
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
+    localStorage.removeItem("teamData");
+    localStorage.setItem("teamData", JSON.stringify(newData));
+  };
+
+  const handleAddTeam = () => {
+    const teamData = JSON.parse(localStorage.getItem("teamData")) || [];
+    const newTeam = {
+      id: teamData.length + 1,
+      name: inputValue,
+      point: 0,
+      image: "",
     };
+    teamData.push(newTeam);
+    setData(teamData);
+    setInputValue("");
+    localStorage.removeItem("teamData");
+    localStorage.setItem("teamData", JSON.stringify(teamData));
+  };
 
-    const handleKeyPress = (event, inputId) => {
-        if (event.key === 'Enter') {
-          if (inputId === 'addTeam') {
-            handleAddTeam(); // Вызываем функцию добавления команды для первого инпута
-          } 
-        }
+  const handleInputChangeRunLine = (event) => {
+    setChangetextRunLine(event.target.value);
+  };
+
+  const handleKeyPress = (event, inputId) => {
+    if (event.key === "Enter") {
+      if (inputId === "addTeam") {
+        handleAddTeam();
+      }
+    }
+  };
+
+  const handleChangeRunLine = () => {
+    setRunLine(true);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredTeams =
+    data &&
+    data.filter((data) =>
+      data.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+  const handleCancelRunLine = () => {
+    setRunLine(false);
+  };
+
+  const handleConfirmRunLine = () => {
+    const newRunLine = {
+      text: ChangetextRunLine,
+      speed: scrollAmount,
     };
+    const runLineData = [newRunLine];
 
-    return (
-        <div className="fullscreen">
-            <div className="logoContainer">
-                <img className="cyberpoligonLogo" src="/images/киберполигон.png" />
-                <input className='inputContainerTeam'
-                        placeholder='название новой команды'
-                        value={inputValue}
-                        onChange={handleInputChange}
-                        onKeyPress={(event) => handleKeyPress(event, 'addTeam')}
-                />
-                <button className='addTeam' onClick={handleAddTeam}> 
-                    добавить команду
-                </button>
-                <div className='line'/>
-                <div className='IninputContainerSearhTeamAndLogo'>
-                    <img src="/images/поиск.svg" className='Searchteam'/>
-                    <input className='inputContainerSearhTeam'
-                            placeholder="Поиск команд"
-                    />
-                </div>
-            </div>
-            <div className='Container'>
-                <p className='TeamTitle'>
-                    СПИСОК КОМАНД
-                </p>
-                <button className='runLineAdmin'>
-                    редактировать бегущую строку
-                </button>
-            </div>
-            {data && data.length > 0 && data.sort((a, b) => b.point - a.point).map((e, id) => (<TeamAdmin id={e.id} pozition={id + 1} name={e.name} point={e.point} ChangePoints={ChangePoints} DeleteTeam={DeleteTeam}/>))}
-            
-            
+    setRunLine(false);
+    setTextRunLine(runLineData);
+    localStorage.removeItem("runLine");
+    localStorage.setItem("runLine", JSON.stringify(runLineData));
+  };
+
+  const handleSpeedChange = () => {
+    setScrollAmount(event.target.value);
+  };
+
+  return (
+    <div className="fullscreen">
+      <div className="logoContainer">
+        <img className="cyberpoligonLogo" src="/images/киберполигон.png" />
+        <input
+          className="inputContainerTeam"
+          placeholder="название новой команды"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyPress={(event) => handleKeyPress(event, "addTeam")}
+        />
+        <button className="addTeam" onClick={handleAddTeam}>
+          добавить команду
+        </button>
+        <div className="line" />
+        <div className="IninputContainerSearhTeamAndLogo">
+          <img src="/images/поиск.svg" className="Searchteam" />
+          <input
+            className="inputContainerSearhTeam"
+            placeholder="Поиск команд"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
         </div>
-    )
+      </div>
+      <div className="Container">
+        <p className="TeamTitle">СПИСОК КОМАНД</p>
+        <button className="runLineAdmin" onClick={handleChangeRunLine}>
+          редактировать бегущую строку
+        </button>
+      </div>
+      {runLine && (
+        <div className="confirmationOverlay">
+          <div className="confirmationUpRunLine">Редактор</div>
+          <div className="confirmationRunDialog">
+            <div className="runLineTextAdminConteiner">
+              <marquee id="runLineTextAdmin" scrollamount={scrollAmount}>
+                {ChangetextRunLine ? ChangetextRunLine : "Предпросмотр"}
+              </marquee>
+            </div>
+            <p className="sped">скорость</p>
+            <select className="Speed" onChange={handleSpeedChange}>
+              <option value="10">1x</option>
+              <option value="20">2x</option>
+              <option value="30">3x</option>
+            </select>
+            <input
+              className="inpitRunLine"
+              type="text"
+              value={ChangetextRunLine}
+              onChange={handleInputChangeRunLine}
+            />
+
+            <div className="ButtonRun">
+              <button className="ConfirmRunSave" onClick={handleConfirmRunLine}>
+                Сохранить изменения
+              </button>
+              <button className="CancelRun" onClick={handleCancelRunLine}>
+                Назад
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {filteredTeams &&
+        filteredTeams.length > 0 &&
+        filteredTeams
+          .sort((a, b) => b.point - a.point)
+          .map((e, id) => (
+            <TeamAdmin
+              id={e.id}
+              pozition={id + 1}
+              name={e.name}
+              point={e.point}
+              ChangePoints={ChangePoints}
+              DeleteTeam={DeleteTeam}
+              ChangeAvatar={ChangeAvatar}
+            />
+          ))}
+    </div>
+  );
 }
 export default AdminPanel;
--- src/components/TeamAdmin.jsx
+++ src/components/TeamAdmin.jsx
@@ -1,65 +1,198 @@
-import React, { useState, useEffect } from 'react';
-import './styles/AdminPanel.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./styles/AdminPanel.css";
+import AvatarEditor from "react-avatar-editor";
 
+function TeamAdmin({
+  id,
+  name,
+  point,
+  pozition,
+  ChangePoints,
+  DeleteTeam,
+  ChangeAvatar,
+}) {
+  const [inputValue, setInputValue] = useState("");
+  const [showDeleteTeam, setShowDeleteTeam] = useState(false);
+  const [image, setImage] = useState(null);
+  const [scale, setScale] = useState(1);
+  const [avatar, setAvatar] = useState(false);
+  const [avatarTeam, setAvatarTeam] = useState(false);
 
-function TeamAdmin({id, name, point, pozition, ChangePoints, DeleteTeam}) {
+  const handleDeleteTeam = () => {
+    setShowDeleteTeam(true);
+  };
 
-    const [inputValue, setInputValue] = useState('');
+  const handleConfirmDelete = () => {
+    DeleteTeam(id);
+    setShowDeleteTeam(false);
+  };
 
-    const handleAddPoint = () => {
-        ChangePoints(id, point + Number(inputValue));
-        setInputValue(''); 
-       
-    };
+  const handleCancelDelete = () => {
+    setShowDeleteTeam(false);
+  };
 
-    const handleDeleteTeam = () => {
-        DeleteTeam(id);
+  const handleAddPoint = () => {
+    ChangePoints(id, point + Number(inputValue));
+    setInputValue("");
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyPress = (event, inputId) => {
+    if (event.key === "Enter") {
+      if (inputId === "addPoint") {
+        handleAddPoint();
+      }
     }
+  };
 
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-    };
+  const handleButtonClick = () => {
+    setAvatar(true);
+  };
 
-    const handleKeyPress = (event, inputId) => {
-        if (event.key === 'Enter') {
-          if (inputId === 'addPoint') {
-            handleAddPoint();
-          } 
-        }
-    };
+  const handleAvatarExit = () => {
+    setAvatar(false);
+  };
 
+  const handleAvatarBack = () => {
+    setAvatarTeam(false);
+    setAvatar(true);
+  };
 
-    return(
-        <div className='ContainerTeam'>
-            <div className='Team'>
-                <p className='NumberTeam'>
-                    {pozition}
-                </p>
-                <img src='/images/Команда 1.webp' className='TeamLogo'/>
-                <div className="NameTeam">
-                    {name}
-                </div>
-                <div className='TeamPoint'>
-                    {point}
-                </div>
-                <div className='plus'>
-                    +
-                </div>
-                <input type="number" 
-                       className='addPoint'
-                       placeholder='Значение'
-                       value={inputValue}
-                       onChange={handleInputChange}
-                       onKeyPress={(event) => handleKeyPress(event, 'addPoint')}
-                />
-                <button className='Add' onClick={handleAddPoint}>
-                    добавить
+  const handleAvatarTeamExit = () => {
+    setAvatarTeam(false);
+  };
+
+  const handleImageUpload = (event) => {
+    setAvatarTeam(true);
+    setAvatar(false);
+    const file = event.target.files[0];
+    setImage(URL.createObjectURL(file));
+  };
+
+  const handleAvatarConfirm = () => {
+    ChangeAvatar(id, image);
+  };
+
+  return (
+    <div className="ContainerTeam">
+      <div className="Team">
+        <p className="NumberTeam">{pozition}</p>
+        <img src="/images/Команда 1.webp" className="TeamLogo" />
+        <div className="NameTeam">{name}</div>
+        <div className="TeamPoint">{point}</div>
+        <div className="plus">+</div>
+        <input
+          type="number"
+          className="addPoint"
+          placeholder="Значение"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyPress={(event) => handleKeyPress(event, "addPoint")}
+        />
+        <button className="Add" onClick={handleAddPoint}>
+          добавить
+        </button>
+        <img
+          src="/images/удаление.svg"
+          className="DeleteTeam"
+          onClick={handleDeleteTeam}
+        />
+        {showDeleteTeam && (
+          <div className="confirmationOverlay">
+            <div className="confirmationUpLine">Удаление команды</div>
+            <div className="confirmationDialog">
+              <p>Вы точно хотите удалить команду?</p>
+              <div className="Button">
+                <button className="ConfirmDelete" onClick={handleConfirmDelete}>
+                  Подтвердить
                 </button>
-                <img src="/images/удаление.svg" className='DeleteTeam' onClick={handleDeleteTeam}/>
-                <img src="/images/добавление.svg" className='AddTeamLogo'/>
+                <button className="CancelDelete" onClick={handleCancelDelete}>
+                  Отмена
+                </button>
+              </div>
+              <div className="confirmationDownLine" />
             </div>
-        </div>
-    )
+          </div>
+        )}
+        <img
+          src="/images/добавление.svg"
+          className="AddTeamLogo"
+          onClick={handleButtonClick}
+        />
+        {avatar && (
+          <div className="confirmationOverlay">
+            <div className="confirmationUpLineAddAvatar">
+              <div>Загрузка новой фотографии</div>
+              <div>
+                <button className="avatarExit" on onClick={handleAvatarExit}>
+                  X
+                </button>
+              </div>
+            </div>
+            <div className="confirmationDialog">
+              <p>Вы можете загрузить изображение в формате JPG,GIF или PNG</p>
+              <label className="customFileUpload">
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageUpload}
+                />
+                Выбрать файл
+              </label>
+              <div className="confirmationDownLine">
+                Если у вас возникают проблемы с загрузкой, попробуйте выбрать
+                фото меньшего размера
+              </div>
+            </div>
+          </div>
+        )}
+        {avatarTeam && (
+          <div className="confirmationOverlay">
+            <div className="confirmationUpLineAvatar">
+              Выбор миниатюры
+              <button className="avatarExit" on onClick={handleAvatarTeamExit}>
+                X
+              </button>
+            </div>
+            <div className="confirmationDialogAvatar">
+              <p>Выберите область для показа миниатюры фотографии</p>
+              <AvatarEditor
+                className="AvatarEditor"
+                image={image}
+                width={250}
+                height={250}
+                border={50}
+                borderRadius={125}
+                scale={scale}
+              />
+              <div className="ScaleConteiner">
+                <input
+                  className="Scale"
+                  type="range"
+                  min="1"
+                  max="2"
+                  step="0.01"
+                  value={scale}
+                  onChange={(event) => setScale(parseFloat(event.target.value))}
+                />
+              </div>
+              <div className="ButtonAvatar">
+                <button className="ConfirmAvatar" onClick={handleAvatarConfirm}>
+                  Сохранить
+                </button>
+                <button className="CancelAvatar" onClick={handleAvatarBack}>
+                  Назад
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default TeamAdmin;
